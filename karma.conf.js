965fa303a02b5df8275cd8a58a91fecdf94134e6
Nuthipong <inwclick17694@gmail.com>
2022-10-07 11:22:17 +0700

    test

--- karma.conf.js
+++ karma.conf.js
@@ -37,6 +37,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
+    browsers: ['ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
